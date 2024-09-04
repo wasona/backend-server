a77a63043011ecdee92f741549473000cf09edb1
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-09-04 12:25:45 +0300

    Allow frontend cors access

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import routes from './routes'; // Import the routes
+import cors from 'cors';
 
 const app = express();
 const port = 30737;
@@ -7,6 +8,10 @@ const port = 30737;
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}))
 
 // Use the routes
 app.use('/', routes);
