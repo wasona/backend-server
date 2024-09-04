21967bdcb09aa7132bf36321840ead3f407d1d1c
Ji Younghyun <younghyun1@gmail.com>
2024-09-04 19:26:33 +0900

    [feature] removed CORS, doing nginx

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import routes from "./routes"; // Import the routes
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 const port = 30737;
@@ -9,16 +9,16 @@ const port = 30737;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-// Use the CORS middleware
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "*",
+//   optionsSuccessStatus: 200,
+// };
+// // Use the CORS middleware
+// app.use(cors(corsOptions));
 
 // Use the routes
 app.use("/", routes);
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
