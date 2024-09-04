7b6dc3a28cf8aa2a6bbd01cd30919302cc24ebb5
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-09-04 11:28:30 +0300

    Try test db connection

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,25 @@
 import { Router, Request, Response } from 'express';
 
+const initOptions = {/* initialization options */};
+const pgp = require('pg-promise')(initOptions);
+const db = pgp(connection);
+
 const router = Router();
 
 // Define a simple route
 router.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
+});
+
+// Test db connection
+router.get('/db', (req: Request, res: Response) => {
+    db.any('SELECT VERSION();', [true])
+    .then(function(data) {
+        res.send(data);
+    })
+    .catch(function(error) {
+        console.log('ERROR:', error)
+    });
 });
 
 // You can define more routes here
