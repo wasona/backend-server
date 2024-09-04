e6b1c8c3e5b89772a609e7e6aee109e4f90762cc
Ji Younghyun <younghyun1@gmail.com>
2024-09-04 18:52:35 +0900

    [config] allow from *

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express';
-import routes from './routes'; // Import the routes
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import routes from "./routes"; // Import the routes
+import cors from "cors";
 
 const app = express();
 const port = 30737;
@@ -8,14 +8,16 @@ const port = 30737;
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
-}))
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  }),
+);
 
 // Use the routes
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
