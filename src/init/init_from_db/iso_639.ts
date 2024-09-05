b4c51d6016c48cc38b46e52696241927456dc755
Ji Younghyun <younghyun1@gmail.com>
2024-09-06 03:06:56 +0900

    [feature] async stuff

--- src/init/init_from_db/iso_639.ts
+++ src/init/init_from_db/iso_639.ts
@@ -1,9 +1,10 @@
 import { Iso639 } from "../../models/db_models/iso-639";
 
 export function fetchIso639List(db: any): Iso639[] {
+  const startTime = Date.now();
   try {
     const data = db.any("SELECT * FROM v1.iso_639;");
-    return data.map((item: any) => {
+    const iso639List = data.map((item: any) => {
       try {
         return new Iso639(
           item.iso_639_2,
@@ -16,6 +17,9 @@ export function fetchIso639List(db: any): Iso639[] {
         throw new Error("Validation Error");
       }
     });
+    const endTime = Date.now();
+    console.log(`Iso639List initiated in ${endTime - startTime}ms`);
+    return iso639List;
   } catch (error) {
     console.log("ERROR:", error);
     throw new Error("Internal Server Error");
